b5d19e91ab4ea987ab6547ef6106de232bcded5d
mirzaal1 <crystalm242@gmail.com>
2023-08-25 14:07:58 +0500

    android data fetched

--- androidApp/src/main/java/com/mirzaal1/kmpimagegallery/android/App.kt
+++ androidApp/src/main/java/com/mirzaal1/kmpimagegallery/android/App.kt
@@ -4,16 +4,18 @@ import android.app.Application
 import com.mirzaal1.kmpimagegallery.android.di.viewModelModule
 import di.initKoin
 import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.loadKoinModules
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        initKoin{
+        initKoin {
             androidContext(this@App)
-            module { viewModelModule }
+            androidLogger()
+            loadKoinModules(viewModelModule)
         }
     }
 }
--- shared/src/commonMain/kotlin/ui/screen/ImagesScreen.kt
+++ shared/src/commonMain/kotlin/ui/screen/ImagesScreen.kt
@@ -1,6 +1,9 @@
 package ui.screen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
@@ -12,11 +15,21 @@ import ui.viewmodel.ImagesGalleryViewModel
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun ImagesScreen(viewModel: ImagesGalleryViewModel){
+fun ImagesScreen(viewModel: ImagesGalleryViewModel) {
     AppTheme {
-        CompositionLocalProvider(){
+        CompositionLocalProvider() {
             val imagesList: Images by viewModel.images.collectAsState()
             val state: State by viewModel.state.collectAsState()
+
+            Button(
+                onClick = {
+                }
+            ) {
+                Text("Hello ali")
+            }
         }
     }
 }
+
+
+
